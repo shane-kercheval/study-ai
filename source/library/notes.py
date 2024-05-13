39ce8003cc9d84c79bd173b896ad866baba44be9
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2024-05-13 10:20:32 -0700

    refactored notes so that each note object is self-contained in terms of subject/note metadata

--- source/library/notes.py
+++ source/library/notes.py
@@ -1,6 +1,7 @@
 # load yaml file
 
 from abc import ABC, abstractmethod
+from copy import deepcopy
 from textwrap import dedent
 from pydantic import BaseModel, field_validator
 from enum import Enum
@@ -15,117 +16,163 @@ class Priority(str, Enum):
     high = 'high'
 
 
-class Note(ABC, BaseModel):
-    priority: Priority = Priority.medium
+class SubjectMetadata(BaseModel):
+    category: str
+    ident: str
+    name: str
+    abbreviation: str
+
+
+class NoteMetadata(BaseModel):
+    source_name: str
+    source_reference: str
     reference: str | None = None
+    priority: Priority = Priority.medium
     tags: list[str] = []
 
-    def uuid(self) -> str:
-        """Return a unique identifier for the note (e.g. a hash of the content)."""
-        content = '-'.join([f"{k}={v}" for k, v in self.dict().items()])
-        return hashlib.sha256(content.encode()).hexdigest()
 
-    @abstractmethod
-    def note(self):
-        """Render the 'description' e.g. the 'answer' or 'definition' to provide more detail."""
-
-class Flashcard(Note):
-    @abstractmethod
-    def preview(self):
-        """Render the 'preview' e.g. the 'question' or 'term' to give context."""
-
-class TextNote(Note):
-    text: str
-
-    @field_validator('text')
-    @classmethod
-    def text_validator(cls, t: str) -> str:
-        return dedent(t).strip()
-
-    def preview(self):
-        return self.text
-
-    def note(self):
-        return self.text
-
-
-class DefinitionNote(Flashcard):
-    term: str
-    definition: str
-
-    @field_validator('term')
-    @classmethod
-    def term_validator(cls, t: str) -> str:
-        return dedent(t).strip()
+class Note(ABC):
     
-    @field_validator('definition')
-    @classmethod
-    def definition_validator(cls, d: str) -> str:
-        return dedent(d).strip()
-
-    def preview(self):
-        return self.term
-
-    def note(self):
-        return self.term + self.definition
-
-
-class QuestionAnswerNote(Flashcard):
-    question: str
-    answer: str
-
-    @field_validator('question')
-    @classmethod
-    def term_validator(cls, q: str) -> str:
-        return dedent(q).strip()
-    
-    @field_validator('answer')
-    @classmethod
-    def definition_validator(cls, a: str) -> str:
-        return dedent(a).strip()
-
-    def preview(self):
-        return self.question
-
-    def note(self):
-        return self.question + self.answer
-
-
-class ClassNote(BaseModel):
-
-    subject_metadata: dict = {}
-    note_metadata: dict = {}
-    note: Note
+    def __init__(self, subject_metadata: SubjectMetadata, note_metadata: NoteMetadata):
+        self.subject_metadata = subject_metadata
+        self.note_metadata = note_metadata
 
     def uuid(self) -> str:
         """Return a unique identifier for the class (e.g. a hash of the content)."""
         subject_meta_content = '-'.join([f"{k}={v}" for k, v in self.subject_metadata.items()])
         note_meta_content = '-'.join([f"{k}={v}" for k, v in self.note_metadata.items()])
-        return hashlib.sha256((subject_meta_content + note_meta_content).encode()).hexdigest() + note.uuid()  # noqa
+        text = subject_meta_content + note_meta_content + self.text()
+        return hashlib.sha256(text.encode()).hexdigest()
 
-def parse(data: dict) -> list[ClassNote]:
-    """Create a list of ClassNote objects from a dictionary."""
+    @abstractmethod
+    def text(self) -> str:
+        """Render all of the text of the note."""
+
+
+class TextNote(Note):
+    """A TextNote is a Note that has only text."""
+    
+    def __init__(self, subject_metadata: SubjectMetadata, note_metadata: NoteMetadata, text: str):
+        super().__init__(subject_metadata=subject_metadata, note_metadata=note_metadata)
+        self._text = dedent(text).strip()
+
+    def text(self) -> str:
+        return self._text
+
+
+class Flashcard(Note):
+    """A Flashcard is a Note that has a 'preview' and a 'answer'."""
+    @abstractmethod
+    def preview(self) -> str:
+        """Render the 'preview' e.g. the 'question' or 'term' to give context."""
+
+    @abstractmethod
+    def answer(self) -> str:
+        """Render the 'answer' or 'definition' of the flashcard."""
+
+
+class DefinitionNote(Flashcard):
+    def __init__(
+            self,
+            subject_metadata: SubjectMetadata, note_metadata: NoteMetadata,
+            term: str, definition: str):
+        super().__init__(subject_metadata=subject_metadata, note_metadata=note_metadata)
+        self._term = dedent(term).strip()
+        self._definition = dedent(definition).strip()
+
+    def preview(self) -> str:
+        return self._term
+    
+    def answer(self) -> str:
+        return self._definition
+
+    def text(self) -> str:
+        return self._term + self._definition
+
+
+class QuestionAnswerNote(Flashcard):
+
+    def __init__(
+            self,
+            subject_metadata: SubjectMetadata, note_metadata: NoteMetadata,
+            question: str, answer: str):
+        super().__init__(subject_metadata=subject_metadata, note_metadata=note_metadata)
+        self._question = dedent(question).strip()
+        self._answer = dedent(answer).strip()
+
+    def preview(self) -> str:
+        return self._question
+    
+    def answer(self) -> str:
+        return self._answer
+    
+    def text(self) -> str:
+        return self._question + self._answer
+    
+
+def parse(data: dict) -> list[Note]:
+    """
+    Creates a list of Note objects from a dictionary/yaml.
+
+    An example yaml file:
+    ```
+    subject_metadata:
+      category: OMSCS
+      ident: CS 6200
+      name: Graduate Introduction to Operating Systems
+      abbreviation: GIOS
+    note_metadata:
+      source_name: Beej's Guide to Network Programming
+      source_reference: https://beej.us/guide/bgnet/pdf/bgnet_usl_c_1.pdf
+      tags:
+        - systems
+        - c
+        - networking
+        - beejs
+    notes:
+      - term: What is a `socket`?
+        definition: A **way to speak to other programs** using standard Unix **file descriptors**.
+        reference: https://beej.us/guide/bgnet/html/#what-is-a-socket; chapter 2
+      - term: What is a `file descriptor`?
+        definition: An **integer** associated with an **open file**.
+        reference: https://beej.us/guide/bgnet/html/#what-is-a-socket; chapter 2
+    ```
+    """
+    data = deepcopy(data)  # don't modify the original data
     notes = []
     for note_dict in data['notes']:
+        reference = note_dict.pop('reference', None)
+        priority = note_dict.pop('priority', Priority.medium)
+        subject_metadata = SubjectMetadata(**data['subject_metadata'])
+        note_metadata = NoteMetadata(
+            **data['note_metadata'] | {'reference': reference, 'priority': priority}
+        )
         if isinstance(note_dict, str):
             note = TextNote(text=note_dict)
         elif isinstance(note_dict, dict):
             if 'text' in note_dict:
-                note = TextNote(**note_dict)
+                note = TextNote(
+                    **note_dict,
+                    subject_metadata=subject_metadata,
+                    note_metadata=note_metadata,
+                )
             elif 'term' in note_dict and 'definition' in note_dict:
-                note = DefinitionNote(**note_dict)
+                note = DefinitionNote(
+                    **note_dict,
+                    subject_metadata=subject_metadata,
+                    note_metadata=note_metadata,
+                )
             elif 'question' in note_dict and 'answer' in note_dict:
-                note = QuestionAnswerNote(**note_dict)
+                note = QuestionAnswerNote(
+                    **note_dict,
+                    subject_metadata=subject_metadata,
+                    note_metadata=note_metadata,
+                )
             else:
                 raise ValueError(f"Invalid note type: {note_dict}")
         else:
             raise ValueError(f"Invalid note type: {note_dict}")
-        class_note = ClassNote(
-            subject_metadata=data['subject_metadata'],
-            note_metadata=data['note_metadata'],
-            note=note,
-        )
-        notes.append(class_note)
+        notes.append(note)
     return notes
 
 
@@ -156,60 +203,60 @@ class History:
         }
 
 
-class TestBank:
-    def __init__(
-            self,
-            notes: list[ClassNote],
-            history: dict[str, History] | None = None,
-            flash_only: bool = False,
-            class_category: str | None = None,
-            class_ident: str | None = None,
-            class_name: str | None = None,
-            class_abbr: str | None = None,
-        ):
-        """TODO."""
-        self.test_bank = {}
-        class_attributes = {
-            'category': class_category,
-            'ident': class_ident,
-            'name': class_name,
-            'abbr': class_abbr
-        }
+# class TestBank:
+#     def __init__(
+#             self,
+#             notes: list[SubjectNote],
+#             history: dict[str, History] | None = None,
+#             flash_only: bool = False,
+#             class_category: str | None = None,
+#             class_ident: str | None = None,
+#             class_name: str | None = None,
+#             class_abbr: str | None = None,
+#         ):
+#         """TODO."""
+#         self.test_bank = {}
+#         class_attributes = {
+#             'category': class_category,
+#             'ident': class_ident,
+#             'name': class_name,
+#             'abbr': class_abbr
+#         }
 
-        for class_note in notes:
-            for note in class_note.notes:
-                if flash_only and not isinstance(note, Flashcard):
-                    continue
-                if any(
-                    class_note.subject_metadata.get(attr) != value
-                    for attr, value in class_attributes.items() if value
-                    ):
-                    continue
-                uuid = class_note.uuid() + note.uuid()
-                assert uuid not in self.test_bank, f"Duplicate UUID: {uuid}"
-                self.test_bank[uuid] = {
-                    'uuid': uuid,
-                    'history': history[uuid] if history and uuid in history else History(),
-                    'note': note,
-                }
+#         for class_note in notes:
+#             for note in class_note.notes:
+#                 if flash_only and not isinstance(note, Flashcard):
+#                     continue
+#                 if any(
+#                     class_note.subject_metadata.get(attr) != value
+#                     for attr, value in class_attributes.items() if value
+#                     ):
+#                     continue
+#                 uuid = class_note.uuid() + note.uuid()
+#                 assert uuid not in self.test_bank, f"Duplicate UUID: {uuid}"
+#                 self.test_bank[uuid] = {
+#                     'uuid': uuid,
+#                     'history': history[uuid] if history and uuid in history else History(),
+#                     'note': note,
+#                 }
 
-    def draw(self) -> dict:
-        """Draw a note from the class notes."""
-        probabilities = {
-            k: v['history'].beta_draw()
-            for k, v in self.test_bank.items()
-        }
-        # softmax probabilities across all values
-        sum_probs = sum(probabilities.values())
-        probabilities = {k: v / sum_probs for k, v in probabilities.items()}
-        # draw a note
-        uuid = np.random.choice(list(probabilities.keys()), p=list(probabilities.values()))
-        return self.test_bank[uuid]
+#     def draw(self) -> dict:
+#         """Draw a note from the class notes."""
+#         probabilities = {
+#             k: v['history'].beta_draw()
+#             for k, v in self.test_bank.items()
+#         }
+#         # softmax probabilities across all values
+#         sum_probs = sum(probabilities.values())
+#         probabilities = {k: v / sum_probs for k, v in probabilities.items()}
+#         # draw a note
+#         uuid = np.random.choice(list(probabilities.keys()), p=list(probabilities.values()))
+#         return self.test_bank[uuid]
 
-    def correct_answer(self, uuid: str, correct: bool) -> None:
-        """Update the history of the note based on the correctness of the answer."""
-        self.test_bank[uuid]['history'].correct_answer(correct)
+#     def correct_answer(self, uuid: str, correct: bool) -> None:
+#         """Update the history of the note based on the correctness of the answer."""
+#         self.test_bank[uuid]['history'].correct_answer(correct)
 
-    @property
-    def history(self) -> dict[str, History]:
-        return {k: v['history'] for k, v in self.test_bank.items()}
+#     @property
+#     def history(self) -> dict[str, History]:
+#         return {k: v['history'] for k, v in self.test_bank.items()}
--- tests/test_notes.py
+++ tests/test_notes.py
@@ -1,6 +1,8 @@
 """Test utilities.py."""
 
-from source.library.notes import parse
+from copy import deepcopy
+from textwrap import dedent
+from source.library.notes import Flashcard, Priority, TextNote, parse
 
 
 def test__parse():
@@ -8,9 +10,34 @@ def test__parse():
     with open("/code/tests/test_files/notes1.yaml") as _handle:
         yaml_data = yaml.safe_load(_handle)
     notes = parse(yaml_data)
-    for note in notes:
-        assert note.subject_metadata == yaml_data['subject_metadata']
-        assert note.note_metadata == yaml_data['note_metadata']
-        assert note.note
-    notes
+    for note, note_dict in zip(notes, yaml_data['notes'], strict=True):
+        # assert actual == expected
+        assert dict(note.subject_metadata) == yaml_data['subject_metadata']
+        # the note_metadata has additional values (e.g. reference, priority) that are found on
+        # individual notes in the dict/yaml
+        note_metadata = deepcopy(dict(note.note_metadata))
+        actual_reference = note_metadata.pop('reference', None)
+        actual_priority = note_metadata.pop('priority', 'medium')
+        # after removing reference/priority, the remaining values should match the original yaml
+        assert note_metadata == yaml_data['note_metadata']
+        if isinstance(note, TextNote):
+            assert note.text() == dedent(note_dict['text']).strip()
+        elif isinstance(note, Flashcard):
+            if 'term' in note_dict:
+                expected_preview = dedent(note_dict['term']).strip()
+                expected_answer = dedent(note_dict['definition']).strip()
+            elif 'question' in note_dict:
+                expected_preview = dedent(note_dict['question']).strip()
+                expected_answer = dedent(note_dict['answer']).strip()
+            else:
+                raise ValueError("Invalid note format.")
+            assert note.preview() == expected_preview
+            assert note.answer() == expected_answer
+            assert note.text() == expected_preview + expected_answer
     
+        assert note.note_metadata.reference == actual_reference
+        assert actual_reference == note_dict.get('reference', None)
+        assert note.note_metadata.priority == Priority[actual_priority]
+        assert Priority[actual_priority] == Priority[note_dict.get('priority', 'medium')]
+        # TODO check tags
+
